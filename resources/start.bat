a75ed3fe16bfc812ffe4483f5cad3dc2b22090ec
omarerrer <95306982+omarerrer@users.noreply.github.com>
2023-07-06 11:59:28 +0200

    Update start.bat

--- resources/start.bat
+++ resources/start.bat
@@ -1,5 +1,4 @@
 @echo off
-color a
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Server 2019 By JE_army" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
