b6776132abcee98d1994af56d610dce2b9bf3ccd
omarerrer <95306982+omarerrer@users.noreply.github.com>
2023-07-06 11:53:37 +0200

    Update start.bat

--- resources/start.bat
+++ resources/start.bat
@@ -16,10 +16,11 @@ ICACLS C:\Windows\installer /grant jearmy:F >nul
 echo Successfully installed! If RDP is dead, rebuild again.
 echo      ██╗███████╗         █████╗ ██████╗ ███╗   ███╗██╗   ██╗
 echo      ██║██╔════╝        ██╔══██╗██╔══██╗████╗ ████║╚██╗ ██╔╝
-echo      ██║█████╗          ███████║██████╔╝██╔████╔██║ ╚████╔╝
+echo      ██║█████╗          ███████║██████╔╝██╔████╔██║ ╚████╔╝ 
 echo ██   ██║██╔══╝          ██╔══██║██╔══██╗██║╚██╔╝██║  ╚██╔╝  
-echo ╚█████╔╝███████╗███████╗██║  ██║██║  ██║██║ ╚═╝ ██║   ██║ 
-echo  ╚════╝ ╚══════╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝   ╚═╝ 
+echo ╚█████╔╝███████╗███████╗██║  ██║██║  ██║██║ ╚═╝ ██║   ██║   
+echo  ╚════╝ ╚══════╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝   ╚═╝   
+echo by omar                                                           
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
 echo Username: jearmy
