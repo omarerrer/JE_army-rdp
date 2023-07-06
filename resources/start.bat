bec72ae0b7286e4f4992b601caaf720b4239161d
omarerrer <95306982+omarerrer@users.noreply.github.com>
2023-07-06 10:58:15 +0200

    Update start.bat

--- resources/start.bat
+++ resources/start.bat
@@ -3,19 +3,19 @@ del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Server 2019 By JE_army" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
-net user je_army dabab@dalam /add >nul
-net localgroup administrators je_army /add >nul
-net user je_army /active:yes >nul
+net user jearmy dalam@2007 /add >nul
+net localgroup administrators jearmy /add >nul
+net user jearmy /active:yes >nul
 net user installer /delete
 diskperf -Y >nul
 sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
-ICACLS C:\Windows\Temp /grant je_army:F >nul
-ICACLS C:\Windows\installer /grant je_army:F >nul
+ICACLS C:\Windows\Temp /grant jearmy:F >nul
+ICACLS C:\Windows\installer /grant jearmy:F >nul
 echo Successfully installed! If RDP is dead, rebuild again.
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
-echo Username: je_army
-echo Password: dabab@dalam
+echo Username: jearmy
+echo Password: dalam@2007
 echo You can login now
 ping -n 10 127.0.0.1 >nul
