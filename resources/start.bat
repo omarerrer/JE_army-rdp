973aa9d135c2dabcf2323e95c14fee9d1a7d32a0
omarerrer <95306982+omarerrer@users.noreply.github.com>
2023-07-06 11:32:41 +0200

    Update start.bat

--- resources/start.bat
+++ resources/start.bat
@@ -1,4 +1,5 @@
 @echo off
+color a
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Server 2019 By JE_army" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
@@ -13,6 +14,12 @@ sc start audiosrv >nul
 ICACLS C:\Windows\Temp /grant jearmy:F >nul
 ICACLS C:\Windows\installer /grant jearmy:F >nul
 echo Successfully installed! If RDP is dead, rebuild again.
+echo      ██╗███████╗         █████╗ ██████╗ ███╗   ███╗██╗   ██╗
+echo      ██║██╔════╝        ██╔══██╗██╔══██╗████╗ ████║╚██╗ ██╔╝
+echo      ██║█████╗          ███████║██████╔╝██╔████╔██║ ╚████╔╝
+echo ██   ██║██╔══╝          ██╔══██║██╔══██╗██║╚██╔╝██║  ╚██╔╝  
+echo ╚█████╔╝███████╗███████╗██║  ██║██║  ██║██║ ╚═╝ ██║   ██║ 
+echo  ╚════╝ ╚══════╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝   ╚═╝ 
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
 echo Username: jearmy
